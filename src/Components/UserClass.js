a43454325b77eeae260c3cda3b3ea4db1b58c500
Student-Anshuman <rajmangalanshu123>
2024-10-31 05:05:34 +0530

    extract props

--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -14,8 +14,10 @@ class UserClass extends React.Component {
   render() {
     return (
       <div className="user-card">
-        <h2>Name :{ this.props.name}</h2>
-        <h3>Location : { this.props.location}</h3>
+        <h2>Name :{this.props.name}</h2>{" "}
+        {/*This is method to extract props by using this.props.name  */}
+        <h3>Location : {this.props.location}</h3>{" "}
+        {/*This is method to extract props by using this.props.location  */}
       </div>
     );
   }
